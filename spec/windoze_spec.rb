439c2af51fe8d64eacab135325b0f07d85992b43
Dr Nic Williams <drnicwilliams@gmail.com>
2010-06-05 03:55:11 +1000

    fixed spec for missing 'shellwords' requirement

--- spec/windoze_spec.rb
+++ spec/windoze_spec.rb
@@ -24,6 +24,7 @@ describe "github/command.rb" do
   it "should require win32/open3 under Windows" do
     mod = Module.new
     mod.should_receive(:require).with("fileutils")
+    mod.should_receive(:require).with("shellwords")
     mod.should_receive(:require).with("win32/open3")
     mod.class_eval @data, @filename
   end
@@ -31,6 +32,7 @@ describe "github/command.rb" do
   it "should blow up if win32/open3 isn't present under Windows" do
     mod = Module.new
     mod.should_receive(:require).with("fileutils")
+    mod.should_receive(:require).with("shellwords")
     mod.should_receive(:require).with("win32/open3").and_return { raise LoadError }
     mod.should_receive(:warn).with("You must 'gem install win32-open3' to use the github command on Windows")
     lambda { mod.class_eval @data, @filename }.should raise_error(SystemExit)
